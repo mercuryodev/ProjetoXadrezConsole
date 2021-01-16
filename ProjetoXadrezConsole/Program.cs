7bbf760bf1b25ffc98af78ee091b5fad14de6eff
Breno Guilherme <breno.ga.duque@gmail.com>
2021-01-16 15:56:47 -0300

    criando classe posição

--- ProjetoXadrezConsole/Program.cs
+++ ProjetoXadrezConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace ProjetoXadrezConsole
 {
@@ -6,7 +7,11 @@ namespace ProjetoXadrezConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+
+            p = new Posicao(3,4);
+
+            Console.WriteLine($"Posicao: {p}");
         }
     }
 }
